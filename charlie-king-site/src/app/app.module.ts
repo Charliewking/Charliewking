ec932b4d4454fc4c1bc9eca48b04f78877791afb
Charlie King <charliewking@outlook.com>
2019-10-04 12:41:07 -0400

    Added image and achievements components

--- charlie-king-site/src/app/app.module.ts
+++ charlie-king-site/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AchievementsComponent } from './achievements/achievements.component';
+import { ImageComponent } from './image/image.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AchievementsComponent,
+    ImageComponent
   ],
   imports: [
     BrowserModule,
